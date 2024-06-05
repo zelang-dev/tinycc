a2f81b25a873299ee701723a7fffb43313ad28d6
Lawrence Stubbs <lstubbs@techno.express>
2024-06-04 22:47:13 -0400

    update .tccVar.bat, .tccVar32.bat and CMakeLists.txt

--- .tccVar32.bat
+++ .tccVar32.bat
@@ -1,7 +1,7 @@
 @echo off
-set LIBRARY_PATH="C:\Program Files (x86)\tcc\lib"
-set C_INCLUDE_PATH="C:\Program Files (x86)\tcc\include"
-set CPATH="C:\Program Files (x86)\tcc\bin"
-set CMAKE_C_COMPILER="C:\Program Files (x86)\tcc\bin\tcc"
+set LIBRARY_PATH=C:\Program Files (x86)\tcc\lib
+set C_INCLUDE_PATH=C:\Program Files (x86)\tcc\include
+set CPATH=C:\Program Files (x86)\tcc\bin
+set CMAKE_C_COMPILER=C:\Program Files (x86)\tcc\bin\tcc
 set CC=tcc
 set "PATH=%PATH%;%CPATH%;c:\Program Files (x86)\CMake\bin"
